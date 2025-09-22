d01ae6455916fd91df6f847a1ca9987ff220ab6e
lliancastro <lianc4793@gmail.com>
2025-09-22 10:07:06 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+# Assign your personal info as string variables
+name = "Lian Candice G. Castro"
+age = "15 years old"
+school = "OB Montessori Center Fv"
+
+# Create a message combining the info
+message = f"My name is {name}.\nI am {age}.\nI study at {school}."
+
+# Print the message to the console (for testing)
+print(message)
